3754381b36ca434c8465915f189a16561d2b7e86
Anthony Salemo <ajssalemo@gmail.com>
2023-11-13 17:50:55 -0500

    testing other ways to log out the cloudevent

--- java/sdk/src/main/java/com/dapr/sdk/Controllers/GetOrdersController.java
+++ java/sdk/src/main/java/com/dapr/sdk/Controllers/GetOrdersController.java
@@ -1,7 +1,5 @@
 package com.dapr.sdk.Controllers;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,8 +13,6 @@ import reactor.core.publisher.Mono;
 // The Dapr SDK for Java allows us to subscribe to a topic and receive messages from it in the same controller
 @RestController
 public class GetOrdersController {
-    private static final Logger log = LoggerFactory.getLogger(GetOrdersController.class);
-
     @Topic(name = "orders", pubsubName = "pubsub")
     @PostMapping("/orders")
     public String getOrders(@RequestBody(required = false) CloudEvent<String> cloudEvent)
@@ -25,7 +21,7 @@ public class GetOrdersController {
 
         Mono.fromRunnable(() -> {
             try {
-                log.info("Subscriber received: " + cloudEvent.getData());
+                System.out.println(cloudEvent.getData());
             } catch (Exception e) {
                 throw new RuntimeException(e);
             }
